b325777a0273ab06c9896b45d3e7c1d405c4f45e
isidoora <113714054+isidoora@users.noreply.github.com>
2024-02-07 21:35:38 +0100

    spell-check crm_analysis.sql

--- crm_analysis.sql
+++ crm_analysis.sql
@@ -1,7 +1,5 @@
-/* data source: https://www.kaggle.com/datasets/jackdaoud/marketing-data?select=ifood_df.csv */
-
-/* analyzing what customer's caracteristics lead to acceptance of campaign */
-/* target variable pilot_campaign has value 1 if customer positively engaged with campaign, 0 otherwise */
+/* analyzing what customer's characteristics lead to acceptance of campaign */
+/* target variable pilot_campaign has value 1 if the customer positively engaged with the campaign, 0 otherwise */
 
 USE marketing_analytics;
 
@@ -12,10 +10,10 @@ SELECT * FROM ifood LIMIT 10;
 ALTER TABLE ifood RENAME COLUMN ï»¿id TO id;
 
 
-/* pilot campagn success by gender */
+/* pilot campaign success by gender */
 SELECT
 	gender,
-    count(*)
+    	COUNT(*)
 FROM ifood
 WHERE pilot_campaign = 1
 GROUP BY 1;
@@ -25,8 +23,8 @@ WITH gender_count AS (
 	SELECT
 		gender, 
 		COUNT(*) AS num
-    FROM ifood
-    GROUP BY 1
+    	FROM ifood
+  	GROUP BY 1
 )
 SELECT
 	DISTINCT ifood.gender,
@@ -39,8 +37,8 @@ JOIN gender_count AS gc
 SELECT
 	CASE
 		WHEN age < 30 THEN "young"
-        WHEN age < 55 THEN "middle-aged"
-        ELSE "elderly"
+        	WHEN age < 55 THEN "middle-aged"
+        	ELSE "elderly"
 	END AS age_cat,
     COUNT(*) AS accepted_pilot
 FROM ifood
@@ -48,12 +46,13 @@ WHERE pilot_campaign = 1
 GROUP BY 1
 ORDER BY 2;
 
-select distinct marital_status from ifood;
+/* checking marital status classification */
+SELECT DISTINCT marital_status FROM ifood;
 
-/* spending by marital status grouped to single/taken*/
+/* spending by marital status grouped to single/taken */
 SELECT 
     CASE
-        WHEN marital_status IN ('Single' , 'Widow', 'Divorced') THEN 'single'
+        WHEN marital_status IN ('Single', 'Widow', 'Divorced') THEN 'single'
         ELSE 'with partner'
     END AS status,
     COUNT(*),
@@ -65,18 +64,18 @@ GROUP BY 1;
 /* success by marital_status of other campaigns*/
 SELECT
 	CASE
-        WHEN marital_status IN ('Single' , 'Widow', 'Divorced') THEN 'single'
-        ELSE 'taken'
-    END status,
-    SUM(campaign1),
-    SUM(campaign2),
-    SUM(campaign3),
-    SUM(campaign4),
-    SUM(campaign5),
-    SUM(pilot_campaign)
+        	WHEN marital_status IN ('Single', 'Widow', 'Divorced') THEN 'single'
+        	ELSE 'taken'
+    	END status,
+	    SUM(campaign1),
+	    SUM(campaign2),
+	    SUM(campaign3),
+	    SUM(campaign4),
+	    SUM(campaign5),
+	    SUM(pilot_campaign)
 FROM ifood
 GROUP BY 1;
--- taken spend more in general and were a lot more influenced by all previous campaigns
+-- taken spend more in general and were a lot more influenced by all previous campaigns but not this one
 
 
 /* average income of customers */
@@ -84,8 +83,9 @@ GROUP BY 1;
 -- output: '51969.8614'
 
 -- successful pilot by level of education
-SELECT education, 
-		COUNT(*) num   
+SELECT
+	education, 
+	COUNT(*) num   
 FROM ifood 
 WHERE pilot_campaign = 1
 GROUP BY education
@@ -98,7 +98,7 @@ SELECT
 		WHEN income < 60000 THEN 'medium'
 		ELSE 'high'
 	END AS income_category,
-    COUNT(*) AS num
+    	COUNT(*) AS num
 FROM ifood
 WHERE pilot_campaign = 1
 GROUP BY 1
@@ -112,11 +112,11 @@ FROM ifood
 WHERE pilot_campaign = 1
 GROUP BY 1;
 
-/* through which channel should we aim next campaign? */
+/* through which channel should we aim the next campaign? */
 SELECT
 	SUM(web_purchases) AS web,
 	SUM(catalog_purchases) AS catalog,
-    SUM(store_purchases) AS store
+    	SUM(store_purchases) AS store
 FROM ifood
 WHERE pilot_campaign = 1;
 
@@ -126,19 +126,19 @@ WITH rfm AS
 	SELECT
 		id,
 		recency,
-		frequancy,
+		frequency,
 		monetary_value,
-		recency * 100 + frequancy * 10 + monetary_value AS rfm_score,
-		ROUND((recency + frequancy + monetary_value)/3) AS rfm_category
+		recency * 100 + frequency * 10 + monetary_value AS rfm_score,
+		ROUND((recency + frequency + monetary_value)/3) AS rfm_category
 	FROM
-    (
+    	(
 		SELECT
 			id,
 			NTILE(5) OVER(ORDER BY last_purchase DESC) AS recency,
-			NTILE(5) OVER(ORDER BY count_purchases) AS frequancy,
+			NTILE(5) OVER(ORDER BY count_purchases) AS frequency,
 			NTILE(5) OVER(ORDER BY amount_spent) AS monetary_value
 		FROM
-        (
+        	(
 			SELECT
 				id,
 				last_purchase,
@@ -152,38 +152,40 @@ WITH rfm AS
 SELECT 
     rfm_category,
     COUNT(*)
-FROM
-    rfm
+FROM rfm
 	LEFT JOIN ifood 
 		ON rfm.id = ifood.id
-WHERE
-    ifood.pilot_campaign = 1
+WHERE ifood.pilot_campaign = 1
 GROUP BY 1
 ORDER BY 2;
 
 -- ----------------------------- ------------------------------- --------------------------- ------------------------------- ------------------------------ --
 /* some queries used to gain insights into data
-that were irelevant for inclusion in final report */
+that were irrelevant for inclusion in the final report */
+
 
 /* spending across food categories */
-SELECT SUM(wines) OVER() AS wines,
-		SUM(fruits) OVER() AS fruit,
+SELECT
+	SUM(wines) OVER() AS wines,
+	SUM(fruits) OVER() AS fruit,
         SUM(meats) OVER() AS meat,
         SUM(fish) OVER() AS fish,
         SUM(sweets) OVER() AS sweet
 FROM ifood
--- WHER pilot_campaign = 1
+-- WHERE pilot_campaign = 1
 LIMIT 1;
 
 /* gold/regular */
-SELECT SUM(gold_products) OVER() AS gold,
-		SUM(regular_products) OVER() AS regular
+SELECT
+	SUM(gold_products) OVER() AS gold,
+	SUM(regular_products) OVER() AS regular
 FROM ifood
 -- WHERE pilot_campaign = 1
 LIMIT 1;
 
 /* spending by gender */
-SELECT gender,
+SELECT
+	gender,
 	SUM(total) AS total
 FROM ifood
 -- WHERE pilot_campaign = 1 
@@ -204,10 +206,10 @@ ORDER BY 2 DESC;
 /* education review */
 SELECT 
 	education,
-    COUNT(*),
-    MIN(income),
-    MAX(income),
-    AVG(income)
+    	COUNT(*),
+    	MIN(income),
+    	MAX(income),
+    	AVG(income)
 FROM ifood
 GROUP BY 1
 ORDER BY 5 DESC;
@@ -215,24 +217,23 @@ ORDER BY 5 DESC;
 /* most valuable customers */
 SELECT
 	id,
-    SUM(total)
+    	SUM(total)
 FROM ifood
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 10;
 
-
 /* age group with most discounted purchases and purchase channels */
 SELECT
 	CASE 
 		WHEN age < 30 THEN "young"
-        WHEN age < 55 THEN "middle aged"
-        ELSE "elderly"
+        	WHEN age < 55 THEN "middle-aged"
+        	ELSE "elderly"
 	END AS age_cat,
-    SUM(web_purchases),
-    SUM(store_purchases),
-    SUM(web_visits_month),
-    SUM(deal_purchases)
+    	SUM(web_purchases),
+    	SUM(store_purchases),
+    	SUM(web_visits_month),
+    	SUM(deal_purchases)
 FROM ifood
 -- WHERE pilot_campaign = 1
 GROUP BY 1;
